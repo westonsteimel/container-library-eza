86b301593a4596555c4e888004bec0f75184a3a0
Weston Steimel <weston.steimel@gmail.com>
2021-08-12 12:06:47 +0100

    update to github actions to perform all updates before building images
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -15,3 +15,16 @@ sed -ri \
 git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}, revision: ${revision}"
 
+set -e
+
+version="master"
+revision=`curl --silent "https://api.github.com/repos/ogham/exa/commits/${version}" | jq .sha | xargs`
+echo "latest edge version: ${version}, revision: ${revision}"
+
+sed -ri \
+    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
+    -e 's/^(ARG REVISION=).*/\1'"\"${revision}\""'/' \
+    "edge/Dockerfile"
+
+git add edge/Dockerfile
+git diff-index --quiet HEAD || git commit --message "updated edge to version ${version}, revision: ${revision}"
